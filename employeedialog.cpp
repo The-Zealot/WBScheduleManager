89c658601d92940a437bb3fca8e835825c3aae6c
The-Zealot <96833407+The-Zealot@users.noreply.github.com>
2023-03-25 13:58:45 +0700

    Change magic const to defines

--- employeedialog.cpp
+++ employeedialog.cpp
@@ -20,9 +20,9 @@ EmployeeDialog::EmployeeDialog(QWidget *parent, QSqlTableModel* model, EmployeeS
     connect(ui->listView, &QListView::doubleClicked, [this](){
         int currentRow = ui->listView->currentIndex().row();
 
-        _employee->name     = _model->data(_model->index(currentRow, 0)).toString();
-        _employee->colorHex = _model->data(_model->index(currentRow, 2)).toString();
-        _employee->salary   = _model->data(_model->index(currentRow, 1)).toUInt();
+        _employee->name     = _model->data(_model->index(currentRow, DB_TABLE_EMPLOYEE_NAME)).toString();
+        _employee->colorHex = _model->data(_model->index(currentRow, DB_TABLE_EMPLOYEE_COLOR)).toString();
+        _employee->salary   = _model->data(_model->index(currentRow, DB_TABLE_EMPLOYEE_SALARY)).toUInt();
 
         this->accept();
     });
