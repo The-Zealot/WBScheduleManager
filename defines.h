69f92386a2ae6fc8acb9df6edfaaeaa6a527126b
The-Zealot <96833407+The-Zealot@users.noreply.github.com>
2023-03-25 13:57:20 +0700

    New header for all defines

---
+++ defines.h
@@ -0,0 +1,14 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define DB_TABLE_EMPLOYEE_NAME      0
+#define DB_TABLE_EMPLOYEE_SALARY    1
+#define DB_TABLE_EMPLOYEE_COLOR     2
+
+#define DB_TABLE_DAYS_DATE          0
+#define DB_TABLE_DAYS_EMPLOYEE      1
+#define DB_TABLE_DAYS_ISFINISHED    2
+#define DB_TABLE_DAYS_ISPAYED       3
+#define DB_TABLE_DAYS_SALARY        4
+
+#endif // DEFINES_H
