4be85410a5c4333517341bde5210d106e28b4a60
The-Zealot <96833407+The-Zealot@users.noreply.github.com>
2023-04-17 00:08:23 +0700

    Added new const after database redesign

--- defines.h
+++ defines.h
@@ -7,6 +7,7 @@
 #define FINISHED_DAY_HEX    "#808080"
 #define PAYED_DAY_HEX       "#00aa00"
 
+#define DB_TABLE_EMPLOYEE_ID        3
 #define DB_TABLE_EMPLOYEE_NAME      0
 #define DB_TABLE_EMPLOYEE_SALARY    1
 #define DB_TABLE_EMPLOYEE_COLOR     2
@@ -16,5 +17,10 @@
 #define DB_TABLE_DAYS_ISFINISHED    2
 #define DB_TABLE_DAYS_ISPAYED       3
 #define DB_TABLE_DAYS_SALARY        4
+
+#define DB_TABLE_POINTS_ID          0
+#define DB_TABLE_POINTS_NAME        1
+#define DB_TABLE_POINTS_MARKET      2
+#define DB_TABLE_POINTS_TABLE       3
 
 #endif // DEFINES_H
